ee6dc962dadd6497a362f0765b9a2263fc8489a4
Nik.San.3452 <nik.san.3452@gmail.com>
2023-01-18 12:04:20 +0500

    Добавлены поля в форму по поиску вакансий, отредактированы шаблоны

--- job_parser/parser/forms.py
+++ job_parser/parser/forms.py
@@ -2,9 +2,23 @@ from django import forms
 
 
 class SearchingForm(forms.Form):
+
     job = forms.CharField(
-        label="Специальность", widget=forms.TextInput(attrs={"placeholder": "Поиск"})
+        label="Что ищем ?", widget=forms.TextInput(attrs={"placeholder": "Поиск"})
     )
     city = forms.CharField(
         label="Город", widget=forms.TextInput(attrs={"placeholder": "Город"})
+    )
+    date_from = forms.DateField(
+        label="Дата от",
+        required=False,
+        widget=forms.DateInput(format="%m/%d/%Y", attrs={"type": "date"}),
+    )
+    date_to = forms.DateField(
+        label="Дата до",
+        required=False,
+        widget=forms.DateInput(format="%m/%d/%Y", attrs={"type": "date"}),
+    )
+    title_search = forms.BooleanField(
+        label="Искать в заголовках вакансий", required=False, widget=forms.CheckboxInput()
     )
