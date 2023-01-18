d82584dfabcfe76e9d26b443b70c3d04f180a449
Nik.San.3452 <nik.san.3452@gmail.com>
2023-01-18 12:03:23 +0500

    Изменены представления домашней страницы и списка вансий

--- job_parser/parser/views.py
+++ job_parser/parser/views.py
@@ -1,60 +1,113 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator
 from django.views import View
-from django.views.generic.base import TemplateView
+from parser.models import City
 
 from parser.forms import SearchingForm
-from parser.mixins import VacancyDataMixin
+from parser.mixins import VacancyDataMixin, FormCheckMixin
 import parsers
 
 
-class HomePageView(TemplateView):
-    template_name = "parser/home.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["form"] = SearchingForm
-        return context
-
-
-class VacancyList(View, VacancyDataMixin):
-    form = SearchingForm()
-
+class HomePageView(View, VacancyDataMixin, FormCheckMixin):
     async def get(self, request, *args, **kwargs):
-        city_from_request = request.GET.get("city")
+        form = SearchingForm()
+
+        context = {
+            "form": form,
+        }
+
+        return render(
+            request=request,
+            template_name="parser/home.html",
+            context=context,
+        )
+
+    async def post(self, request, *args, **kwargs):
+        form = SearchingForm(request.POST)
+
+        city_from_request = request.POST.get("city")
         if city_from_request:
             city_from_request = city_from_request.lower()
 
-        job_from_request = request.GET.get("job")
+        job_from_request = request.POST.get("job")
         if job_from_request:
             job_from_request = job_from_request.lower()
 
-        if (  # Если список вакансий пуст
-            VacancyDataMixin.job_list is None
-            # или город из формы не равен городу из предыдущего запроса
-            or city_from_request != VacancyDataMixin.city
-            # или вакансия из формы не равна вакансии из предыдущего запроса
-            or job_from_request != VacancyDataMixin.job
-        ):  # Получаем список вакансий
-            VacancyDataMixin.job_list.clear()
-            VacancyDataMixin.job_list = await parsers.run(
-                city=city_from_request, job=job_from_request
-            )
-            print(VacancyDataMixin.job_list)
-            # Присваиваем временным переменным значения текущего города и вакансии
-            VacancyDataMixin.city = city_from_request
-            VacancyDataMixin.job = job_from_request
-            
+        date_from = request.POST.get("date_from")
+        date_to = request.POST.get("date_to")
+        title_search = request.POST.get("title_search")
+
+        await self.check_form(
+            city=city_from_request,
+            job=job_from_request,
+            date_from=date_from,
+            date_to=date_to,
+            title_search=title_search,
+        )
+
+        context = {
+            "form": form,
+        }
+
+        return render(
+            request=request,
+            template_name="parser/home.html",
+            context=context,
+        )
+
+
+class VacancyList(View, VacancyDataMixin, FormCheckMixin):
+    async def get(self, request, *args, **kwargs):
+        form = SearchingForm()
+
+        context = {
+            "object_list": VacancyDataMixin.job_list,
+            "form": form,
+        }
+
+        paginator = Paginator(VacancyDataMixin.job_list, 5)
+        page_number = request.GET.get("page")
+        page_obj = paginator.get_page(page_number)
+        context["object_list"] = page_obj
+
+        return render(
+            request=request,
+            template_name="parser/list.html",
+            context=context,
+        )
+
+    async def post(self, request, *args, **kwargs):
+        form = SearchingForm(request.POST)
+
+        city_from_request = request.POST.get("city")
+        if city_from_request:
+            city_from_request = city_from_request.lower()
+
+        job_from_request = request.POST.get("job")
+        if job_from_request:
+            job_from_request = job_from_request.lower()
+
+        date_from = request.POST.get("date_from")
+        date_to = request.POST.get("date_to")
+        title_search = request.POST.get("title_search")
+
+        await self.check_form(
+            city=city_from_request,
+            job=job_from_request,
+            date_from=date_from,
+            date_to=date_to,
+            title_search=title_search,
+        )
+
         context = {
             "object_list": VacancyDataMixin.job_list,
             "city": VacancyDataMixin.city,
             "job": VacancyDataMixin.job,
-            "form": self.form,
+            "form": form,
         }
-        
-        print(VacancyDataMixin.job, job_from_request)
+
         paginator = Paginator(VacancyDataMixin.job_list, 5)
-        page_number = request.GET.get("page")
+        page_number = request.POST.get("page")
         page_obj = paginator.get_page(page_number)
         context["object_list"] = page_obj
 
