051d2b1e087d7f39c6c8c757d13560ca807194f0
Nik.San.3452 <nik.san.3452@gmail.com>
2023-01-19 14:49:49 +0500

    Добавлена модель профиля пользователя

--- job_parser/profiles/models.py
+++ job_parser/profiles/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    city = models.CharField(max_length=255, null=True, blank=True)
+    job = models.CharField(max_length=255, null=True, blank=True)
+    subscribe = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.user)
