cfc37618ca800ae1ad79efffddf6689ea5de6f81
Nik.San.3452 <nik.san.3452@gmail.com>
2023-01-28 15:45:46 +0500

    Созданы пути для представлений добавления и удаления вакансий

--- job_parser/parser/urls.py
+++ job_parser/parser/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from . import views 
+from . import views
 
 urlpatterns = [
-path('', views.HomePageView.as_view(), name='home'),
-path('list/', views.VacancyList.as_view(), name='list'),
+    path("", views.HomePageView.as_view(), name="home"),
+    path("list/", views.VacancyList.as_view(), name="list"),
+    path("favourite/", views.add_to_favourite_view, name="favourite"),
+    path("delete-favourite/", views.delete_from_favourite_view, name="delete_favourite"),
 ]
