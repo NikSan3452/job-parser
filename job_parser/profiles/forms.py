a15470533d98c6fe9980aceb6f84a80b58d53533
Nik.San.3452 <nik.san.3452@gmail.com>
2023-01-29 15:50:47 +0500

    Убрано ограничение required с флажка подписки

--- job_parser/profiles/forms.py
+++ job_parser/profiles/forms.py
@@ -5,5 +5,5 @@ class ProfileForm(forms.Form):
     city = forms.CharField(label="Город", widget=forms.TextInput())
     job = forms.CharField(label="Вакансия", widget=forms.TextInput())
     subscribe = forms.BooleanField(
-        label="Подписаться на рассылку", widget=forms.CheckboxInput()
+        label="Подписаться на рассылку", required=False, widget=forms.CheckboxInput()
     )
