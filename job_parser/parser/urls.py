2e1ddebf107ae4f8c246a5a8782c121a8c35f23b
Nik.San.3452 <nik.san.3452@gmail.com>
2023-02-04 15:33:36 +0500

    Добавлен url черного списка

--- job_parser/parser/urls.py
+++ job_parser/parser/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("list/", views.VacancyList.as_view(), name="list"),
     path("favourite/", views.add_to_favourite_view, name="favourite"),
     path("delete-favourite/", views.delete_from_favourite_view, name="delete_favourite"),
+    path("add-to-black-list/", views.add_to_black_list_view, name="add_to_black_list"),
 ]
