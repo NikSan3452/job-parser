014d8c0cd443f7aa64bb2a6ca39be31dd97fd08b
Nik.San.3452 <nik.san.3452@gmail.com>
2023-02-04 15:29:40 +0500

    Добавлен новый миксин

--- job_parser/parser/mixins.py
+++ job_parser/parser/mixins.py
@@ -1,3 +1,7 @@
+from django.contrib import auth
+from parser.models import FavouriteVacancy, VacancyBlackList
+
+
 class VacancyDataMixin:
     """Класс хранит в памяти параметры поиска и информацию о найденных вакансиях"""
 
@@ -9,3 +13,53 @@ class VacancyDataMixin:
     experience: int = None
 
     job_list: list[dict] = []
+
+
+class VacancyHelpersMixin:
+    """Класс предоставляет вспомогательные методы"""
+
+    async def check_vacancy_black_list(
+        self, vacancies: list[dict], request: dict
+    ) -> list[dict]:
+        """Проверяет добавлена ли вакансия в черный список
+        и есла да, то удаляет ее из выдачи и избранного.
+
+        Args:
+            vacancies (list[dict]): Список вакансий из API.
+            request (dict): Запрос.
+
+        Returns:
+            list[dict]: Список вакансий без добавленных в черный список.
+        """
+        try:
+            user = auth.get_user(request)
+
+            for vacancy in vacancies.copy():
+                async for job in VacancyBlackList.objects.all():
+                    if vacancy["url"] == job.url:
+                        try:
+                            FavouriteVacancy.objects.filter(
+                                user=user, url=vacancy["url"]
+                            ).delete()
+                        except:
+                            pass
+                        vacancies.remove(vacancy)
+            return vacancies
+        except Exception as exc:
+            print(f"Ошибка базы данных {exc}")
+
+    async def get_favourite_vacancy(self, request):
+        """Получает список вакансий добавленных в избранное.
+
+        Args:
+            request (_type_): Запрос.
+
+        Returns:
+            _type_: Список вакансий добавленных в избранное.
+        """
+        try:
+            user = auth.get_user(request)
+            list_favourite = FavouriteVacancy.objects.filter(user=user)
+            return list_favourite
+        except Exception as exc:
+            print(f"Ошибка базы данных {exc}")
