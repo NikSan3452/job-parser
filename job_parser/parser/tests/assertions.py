8f41509d069843c9a618da539aace23f42759d92
Nik.San.3452 <nik.san.3452@gmail.com>
2023-02-16 11:55:56 +0500

    Добавлены новые проверки

--- job_parser/parser/tests/assertions.py
+++ job_parser/parser/tests/assertions.py
@@ -1,9 +1,21 @@
-import httpx
+from django.http import HttpResponse
 
 
 class Assertions:
     @staticmethod
-    def assert_code_status(response: httpx.Response, expected_status_code: int):
+    def assert_code_status(response: HttpResponse, expected_status_code: int):
         assert (
             response.status_code == expected_status_code
         ), f"Неожиданный код ответа, ожидалось {expected_status_code}, получили {response.status_code}"
+
+    @staticmethod
+    def assert_add_vacancy_to_favorite(vacancy: bool):
+        assert vacancy == True, f"Вакансия не была добавлена в избранное"
+
+    @staticmethod
+    def assert_delete_vacancy_from_favourite(vacancy: bool):
+        assert vacancy == False, f"Вакансия не была удалена из избранного"
+
+    @staticmethod
+    def assert_add_vacancy_to_black_list(vacancy: bool):
+        assert vacancy == True, f"Вакансия не была добавлена в черный список"
