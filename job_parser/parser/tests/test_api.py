8b78f164b809817b85a83fcb2006d1134464feef
Nik.San.3452 <nik.san.3452@gmail.com>
2023-02-19 16:13:20 +0500

    Добавлены тесты к странице со списком вакансий

--- job_parser/parser/tests/test_api.py
+++ job_parser/parser/tests/test_api.py
@@ -4,10 +4,8 @@ import pytest
 from django.test import Client
 from django.contrib.auth.models import User
 from parser.models import FavouriteVacancy, VacancyBlackList
+from django.test import Client
 from assertions import Assertions
-
-BASE_URL: str = "http://127.0.0.1:8000"
-POSITIVE_STATUS_CODE: int = 200
 
 
 @pytest.mark.django_db
@@ -18,8 +16,8 @@ class TestHomePage:
         Args:
             client (Client): Клиент.
         """
-        response = client.get(path=f"{BASE_URL}")
-        assert response.status_code == POSITIVE_STATUS_CODE
+        response = client.get(path="/")
+        assert response.status_code == 200
 
 
 @pytest.mark.django_db
@@ -32,17 +30,55 @@ class TestVacancyListPage:
 
     def setup_method(self) -> None:
         """Создает тестового пользователя и входит под ним."""
-        self.user = User.objects.create_user(
-            username="testuser", password="testpass"
-        )
+        self.user = User.objects.create_user(username="testuser", password="testpass")
         self.client.force_login(self.user)
+
+    def test_vacancy_list_method_get(self, mocker):
+        """Тестирует GET запросы к представлению списка вакансий."""
+        mocker.patch(
+            "parser.mixins.VacancyHelpersMixin.get_data_from_cache", return_value=[]
+        )
+
+        client = Client()
+        response = client.get(path="/list/")
+
+        assert response.status_code == 200
+        assert "form" in response.context
+        assert "object_list" in response.context
+        assert "list_favourite" in response.context
+
+    def test_vacancy_list_method_post(self):
+        """Тестирует POST запросы к представлению списка вакансий."""
+        client = Client()
+        data = {
+            "city": "Москва",
+            "job": "Python",
+            "date_from": "2022-01-01",
+            "date_to": "2022-01-31",
+            "title_search": "Python",
+            "experience": "0",
+            "remote": False,
+        }
+
+        response = client.post(path="/list/", data=data)
+
+        assert response.status_code == 200
+        assert "city" in response.context
+        assert "date_from" in response.context
+        assert "date_to" in response.context
+        assert "title_search" in response.context
+        assert "experience" in response.context
+        assert "remote" in response.context
+        assert "form" in response.context
+        assert "object_list" in response.context
+        assert "list_favourite" in response.context
 
     def test_add_to_favourite_view(self) -> None:
         """Тестирует представление добавления вакансии в избранное."""
         data = {"url": self.vacancy_url, "title": self.vacancy_title}
 
         response = self.client.post(
-            path=f"{BASE_URL}/favourite/",
+            path="/favourite/",
             data=json.dumps(data),
             content_type="application/json",
         )
@@ -51,7 +87,7 @@ class TestVacancyListPage:
             user=self.user, url=self.vacancy_url
         ).exists()
 
-        Assertions.assert_code_status(response, POSITIVE_STATUS_CODE)
+        Assertions.assert_code_status(response, 200)
         Assertions.assert_add_vacancy_to_favorite(vacancy)
 
     def test_delete_from_favourite_view(self) -> None:
@@ -63,7 +99,7 @@ class TestVacancyListPage:
         )
 
         response = self.client.post(
-            path=f"{BASE_URL}/delete-favourite/",
+            path="/delete-favourite/",
             data=json.dumps(data),
             content_type="application/json",
         )
@@ -72,7 +108,7 @@ class TestVacancyListPage:
             user=self.user, url=self.vacancy_url
         ).exists()
 
-        Assertions.assert_code_status(response, POSITIVE_STATUS_CODE)
+        Assertions.assert_code_status(response, 200)
         Assertions.assert_delete_vacancy_from_favourite(vacancy)
 
     def test_add_to_black_list_view(self) -> None:
@@ -80,7 +116,7 @@ class TestVacancyListPage:
         data = {"url": self.vacancy_url}
 
         response = self.client.post(
-            path=f"{BASE_URL}/add-to-black-list/",
+            path="/add-to-black-list/",
             data=json.dumps(data),
             content_type="application/json",
         )
@@ -89,5 +125,5 @@ class TestVacancyListPage:
             user=self.user, url=self.vacancy_url
         ).exists()
 
-        Assertions.assert_code_status(response, POSITIVE_STATUS_CODE)
+        Assertions.assert_code_status(response, 200)
         Assertions.assert_add_vacancy_to_black_list(vacancy)
