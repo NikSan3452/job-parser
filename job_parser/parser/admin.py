54f835ba82562fdbba964ff2fefbe0f1b4017bf2
Nik.San.3452 <nik.san.3452@gmail.com>
2023-02-22 12:25:03 +0500

    Убран лишний код

--- job_parser/parser/admin.py
+++ job_parser/parser/admin.py
@@ -1,23 +1,10 @@
 from django.contrib import admin
 
-from parser.models import City, Vacancy
+from parser.models import City
 
 
 @admin.register(City)
 class CityAdmin(admin.ModelAdmin):
     fields = ("city",)
-    # prepopulated_fields = {"slug": ("city",)}
 
 
-@admin.register(Vacancy)
-class VacancyAdmin(admin.ModelAdmin):
-    fields = (
-        "user",
-        "url",
-        "title",
-        "salary",
-        "company",
-        "description",
-        "city",
-        "published_at",
-    )
