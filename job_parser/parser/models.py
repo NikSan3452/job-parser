6da9d7421564301383685c0f295ef2a71bd8b0da
Nik.San.3452 <nik.san.3452@gmail.com>
2023-02-25 13:23:52 +0500

    Доработа модель скрапера

--- job_parser/parser/models.py
+++ job_parser/parser/models.py
@@ -7,6 +7,7 @@ class City(models.Model):
     city = models.CharField(max_length=255, verbose_name="Город", unique=True)
 
     class Meta:
+        app_label = "parser"
         verbose_name = "Город"
         verbose_name_plural = "Города"
 
@@ -15,14 +16,16 @@ class City(models.Model):
 
 
 class VacancyScraper(models.Model):
+    job_board = models.CharField(max_length=255, verbose_name="Площадка")
     url = models.URLField(null=False)
-    title = models.CharField(max_length=250, verbose_name="Вакансия")
+    title = models.CharField(max_length=255, null=True, verbose_name="Вакансия")
     description = models.TextField(null=True, verbose_name="Описание вакансии")
-    city = models.CharField(max_length=250, verbose_name="Город")
-    salary_from = models.CharField(max_length=50, null=True, verbose_name="Зарплата от")
-    salary_to = models.CharField(max_length=50, null=True, verbose_name="Зарплата до")
-    company = models.CharField(max_length=250, verbose_name="Компания")
+    city = models.CharField(max_length=255, null=True, verbose_name="Город")
+    salary = models.CharField(max_length=255, null=True, verbose_name="Зарплата")
+    company = models.CharField(max_length=255, null=True, verbose_name="Компания")
     experience = models.CharField(max_length=100, null=True, verbose_name="Опыт работы")
+    type_of_work = models.CharField(max_length=255, null=True, verbose_name="Тип занятости")
+    remote = models.BooleanField(default=False, null=True, verbose_name="Удаленная работа")
     published_at = models.DateField(auto_now_add=True, verbose_name="Дата публикации")
 
     class Meta:
@@ -35,17 +38,13 @@ class VacancyScraper(models.Model):
 
 
 class VacancyCelery(models.Model):
-    user = models.ForeignKey(
-        Profile, on_delete=models.CASCADE, verbose_name="Пользователь"
-    )
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, verbose_name="Пользователь")
     url = models.URLField(null=False)
     title = models.CharField(max_length=250, verbose_name="Вакансия")
     salary_from = models.CharField(max_length=50, null=True, verbose_name="Зарплата от")
     salary_to = models.CharField(max_length=50, null=True, verbose_name="Зарплата до")
     company = models.CharField(max_length=250, verbose_name="Компания")
-    description = models.TextField(
-        max_length=5000, null=True, verbose_name="Описание вакансии"
-    )
+    description = models.TextField(max_length=5000, null=True, verbose_name="Описание вакансии")
     city = models.CharField(max_length=250, verbose_name="Город")
     published_at = models.DateField(auto_now_add=True, verbose_name="Дата публикации")
 
