d7c775a9959097d1ac643a5d7b8414a31978dc22
Nik.San.3452 <nik.san.3452@gmail.com>
2023-03-02 14:30:22 +0500

    Исправлен баг с созданием профиля

--- job_parser/profiles/signals.py
+++ job_parser/profiles/signals.py
@@ -1,14 +1,15 @@
-from django.db.models.signals import post_save
+from allauth.account.signals import user_signed_up
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import Profile
+
 from logger import setup_logging, logger
 
 # Логирование
 setup_logging()
 
 
-@receiver(post_save, sender=User)
+@receiver(user_signed_up, sender=User)
 def create_profile(sender: User, **kwargs) -> Profile:
     """Создает профиль после регистрации пользователя.
 
@@ -19,9 +20,10 @@ def create_profile(sender: User, **kwargs) -> Profile:
     Returns:
         Profile: Профиль пользователя.
     """
-    user = kwargs["instance"]
+    user = kwargs["user"]
     try:
         profile = Profile.objects.get_or_create(user=user)
+        logger.debug(f"Создан профиль {user}")
     except Exception as exc:
         logger.exception(exc)
     return profile
