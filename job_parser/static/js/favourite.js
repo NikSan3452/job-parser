ef9c73b4245e313ddad7c622c5745ca618210943
Nik.San.3452 <nik.san.3452@gmail.com>
2023-03-09 14:25:30 +0500

    Добавлены функции удаления из черного списка и скрытых вакансий

--- job_parser/static/js/favourite.js
+++ job_parser/static/js/favourite.js
@@ -32,7 +32,7 @@ function addToFavourite(index, vacancyUrl, vacancyTitle) {
     }
 }
 
-function addVacancyToBlackList(index, vacancyUrl) {
+function addVacancyToBlackList(index, vacancyUrl, vacancyTitle) {
     let vacancy = document.getElementById(`delete-vacancy-${index}`);
     vacancy.remove();
     // Получить текущее значение total_vacancies
@@ -41,7 +41,7 @@ function addVacancyToBlackList(index, vacancyUrl) {
         totalVacanciesElement.textContent.split(": ")[1]
     );
     // Обновить значение total_vacancies и обновить элемент DOM
-    totalVacanciesElement.textContent = `По Вашему запросу найдено: ${
+    totalVacanciesElement.textContent = `Количество вакансий по Вашему запросу: ${
         currentTotalVacancies - 1
     }`;
     fetch("/add-to-black-list/", {
@@ -51,12 +51,50 @@ function addVacancyToBlackList(index, vacancyUrl) {
             "X-Requested-With": "XMLHttpRequest",
             "X-CSRFToken": getCookie("csrftoken"),
         },
-        body: JSON.stringify({ url: vacancyUrl }),
+        body: JSON.stringify({ url: vacancyUrl, title: vacancyTitle }),
     })
         .then((response) => response.json())
         .then((data) => {
             console.log(data);
         });
+}
+
+function removeVacancyFromBlackList(index, vacancyUrl) {
+    const vacancy = document.getElementById(
+        `btn-delete-from-black-list-${index}`
+    );
+    console.log(vacancy);
+    vacancy.remove();
+    fetch("delete-from-blacklist/", {
+        method: "POST",
+        credentials: "same-origin",
+        headers: {
+            "X-Requested-With": "XMLHttpRequest",
+            "X-CSRFToken": getCookie("csrftoken"),
+        },
+        body: JSON.stringify({ url: vacancyUrl }),
+    })
+        .then((response) => response.json())
+        .then((data) => {});
+}
+
+function removeCompanyFromHiddenList(index, company) {
+    const vacancy = document.getElementById(
+        `btn-delete-from-hidden-companies-${index}`
+    );
+    console.log(vacancy);
+    vacancy.remove();
+    fetch("delete-from-hidden-companies/", {
+        method: "POST",
+        credentials: "same-origin",
+        headers: {
+            "X-Requested-With": "XMLHttpRequest",
+            "X-CSRFToken": getCookie("csrftoken"),
+        },
+        body: JSON.stringify({ name: company }),
+    })
+        .then((response) => response.json())
+        .then((data) => {});
 }
 
 function removeFavouriteFromProfile(index, vacancyUrl) {
