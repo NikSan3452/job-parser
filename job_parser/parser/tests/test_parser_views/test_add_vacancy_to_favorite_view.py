6fdf65c8501b9950eaa8109d088d8e7345d5faf5
nik.san.3452@gmail.com <nik.san.3452@gmail.com>
2023-04-16 15:47:04 +0500

    Добавлены тесты представления AddVacancyToFavouritesView

---
+++ job_parser/parser/tests/test_parser_views/test_add_vacancy_to_favorite_view.py
@@ -0,0 +1,218 @@
+import json
+from parser.models import FavouriteVacancy
+
+import pytest
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.test import Client
+
+
+@pytest.mark.django_db(transaction=True)
+class TestAddVacancyToFavouritesView:
+    """
+    Класс описывает тестовые случаи для представления AddVacancyToFavouritesView.
+    Декоратор `@pytest.mark.django_db` указывает pytest на необходимость использования
+    базы данных.
+    Параметр `transaction=True` указывает на использование транзакций для ускорения
+    и изоляции тестов.
+    """
+
+    @pytest.fixture
+    def data(self) -> dict:
+        """Фикстура для создания данных для теста.
+
+        Returns:
+            dict: Словарь с данными для теста
+        """
+        vacancy_url = "https://example.com/vacancy/1"
+        vacancy_title = "Test Vacancy"
+        data = {"url": vacancy_url, "title": vacancy_title}
+        return data
+
+    @pytest.fixture
+    def logged_in_client(self, client: Client, test_user: User) -> Client:
+        """Фикстура для предоставления экземпляра клиента Django
+        с аутентифицированным пользователем.
+
+        Эта фикстура использует фикстуры `client` и `test_user` для создания
+        экземпляра клиента Django и аутентификации пользователя.
+        Затем она возвращает экземпляр клиента для использования в тестах.
+
+        Args:
+            client (Client): Фикстура, предоставляющая экземпляр клиента Django.
+            test_user (User): Фикстура, предоставляющая экземпляр пользователя.
+
+        Returns:
+            Client: Экземпляр клиента Django с аутентифицированным пользователем.
+        """
+        client.force_login(test_user)
+        return client
+
+    def test_add_vacancy_to_favourites_view(
+        self, logged_in_client: Client, test_user: User, data: dict
+    ) -> None:
+        """Тест для проверки добавления вакансии в избранное аутентифицированным
+        пользователем.
+
+        В этом тесте выполняется аутентификация пользователя с помощью метода
+        `force_login`, затем отправляется POST-запрос на добавление вакансии
+        в избранное.
+        После этого проверяется статус ответа и наличие добавленной вакансии
+        в избранном.
+
+
+        Args:
+            logged_in_client (Client): Фикстура, предоставляющая экземпляр
+            клиента Django c аутентифицированным пользователем.
+            test_user (User): Фикстура, предоставляющая экземпляр пользователя.
+            data (dict): Словарь с данными для теста.
+        """
+        # Отправка POST-запроса на представление AddVacancyToFavouritesView
+        response = logged_in_client.post(
+            "/favourite/",
+            data=json.dumps(data),
+            content_type="application/json",
+            follow=True,
+        )
+
+        # Проверка ответа и создания объекта FavouriteVacancy
+        assert response.status_code == 200
+        assert FavouriteVacancy.objects.filter(
+            user=test_user, url=data["url"], title=data["title"]
+        ).exists()
+
+    def test_add_vacancy_to_favourites_view_unauthenticated_user(
+        self, client: Client, data: dict
+    ) -> None:
+        """Тест для проверки добавления вакансии в избранное
+        неаутентифицированным пользователем.
+
+        В этом тесте отправляется POST-запрос на добавление вакансии в избранное
+        без аутентификации пользователя.
+        После этого проверяется статус ответа и URL перенаправления.
+
+
+        Args:
+            client (Client): Фикстура, предоставляющая экземпляр клиента Django
+            для выполнения запросов.
+            data (dict): Фикстура, предоставляющая экземпляр пользователя.
+        """
+        response = client.post(
+            "/favourite/",
+            data=json.dumps(data),
+            content_type="application/json",
+        )
+        assert response.status_code == 302
+        assert response.url == "/accounts/login/?next=/favourite/"
+
+    def test_add_vacancy_to_favourites_view_json_decode_error(
+        self, logged_in_client: Client
+    ) -> None:
+        """Тест для проверки обработки невалидного JSON при добавлении вакансии
+        в избранное.
+
+        В этом тесте отправляется POST-запрос на добавление вакансии в избранное
+        с невалидным JSON в теле запроса.
+        После этого проверяется статус ответа.
+
+        Args:
+            logged_in_client (Client): Фикстура, предоставляющая экземпляр
+            клиента Django c аутентифицированным пользователем.
+        """
+        # Отправка POST-запроса с невалидным JSON
+        response = logged_in_client.post(
+            "/favourite/",
+            data="invalid_json",
+            content_type="application/json",
+        )
+        assert response.status_code == 400
+
+    def test_add_vacancy_to_favourites_view_missing_fields(
+        self, logged_in_client: Client
+    ) -> None:
+        """Тест для проверки обработки неполных данных при добавлении вакансии
+        в избранное.
+
+        В этом тесте отправляется POST-запрос на добавление вакансии в избранное
+        с неполными данными (только URL без заголовка).
+        После этого проверяется статус ответа.
+
+        Args:
+            logged_in_client (Client): Фикстура, предоставляющая экземпляр
+            клиента Django c аутентифицированным пользователем.
+            test_user (User): Фикстура, предоставляющая экземпляр пользователя
+            для тестирования.
+        """
+        # Отправка POST-запроса с неполными данными
+        response = logged_in_client.post("/favourite/", {"url": "https://example.com"})
+        assert response.status_code == 400
+
+    def test_add_vacancy_to_favourites_view_wrong_method(
+        self, logged_in_client: Client, data: dict
+    ) -> None:
+        """Тест для проверки обработки неправильного HTTP-метода при добавлении
+        вакансии в избранное.
+
+        В этом тесте отправляется GET-запрос на добавление вакансии в избранное,
+        хотя представление обрабатывает только POST-запросы.
+        После этого проверяется статус ответа.
+
+        Args:
+            logged_in_client (Client): Фикстура, предоставляющая экземпляр пользователя
+            для тестирования.
+            data (dict): Фикстура, предоставляющая данные для отправки в запросе.
+        """
+        response = logged_in_client.get(
+            "/favourite/",
+            data=data,
+        )
+        assert response.status_code == 405
+
+    def test_add_vacancy_to_favourites_view_exceptions(
+        self, logged_in_client: Client, data: dict, mocker
+    ) -> None:
+        """Тест для проверки обработки исключений при добавлении вакансии в избранное.
+
+        В этом тесте сначала создается заглушка (mock) для метода `aget_or_create`
+        модели `FavouriteVacancy`, которая вызывает исключение `IntegrityError`.
+        Затем отправляется POST-запрос на добавление вакансии в избранное
+        и проверяется статус ответа, а также содержимое ответа в формате JSON.
+
+        После этого создается новая заглушка для метода `aget_or_create`,
+        которая вызывает исключение `Exception`.
+        Затем снова отправляется POST-запрос на добавление вакансии в избранное
+        и проверяется статус ответа, а также содержимое ответа в формате JSON.
+
+        Args:
+            logged_in_client (Client): Фикстура, предоставляющая экземпляр пользователя
+            для тестирования.
+            data (dict): Фикстура, предоставляющая данные для отправки в запросе.
+            mocker: Фикстура для создания заглушек (mocks) и подмены вызовов функций.
+        """
+        # Проверка вызова исключения IntegrityError
+        mocker.patch(
+            "parser.models.FavouriteVacancy.objects.aget_or_create",
+            side_effect=IntegrityError,
+        )
+        response = logged_in_client.post(
+            "/favourite/",
+            data=data,
+            content_type="application/json",
+        )
+        assert response.status_code == 400
+        assert response.json() == {"Ошибка": "Такая вакансия уже есть в избранном"}
+
+        # Проверка вызова исключения Exception
+        mocker.patch(
+            "parser.models.FavouriteVacancy.objects.aget_or_create",
+            side_effect=Exception,
+        )
+        response = logged_in_client.post(
+            "/favourite/",
+            data=data,
+            content_type="application/json",
+        )
+        assert response.status_code == 400
+        assert response.json() == {
+            "Ошибка": "При добавлении вакансии в избранное произошла непредвиденная ошибка"
+        }
