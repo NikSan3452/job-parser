69eb5f0bda6874474be36985b21fc665546d4086
nik.san.3452@gmail.com <nik.san.3452@gmail.com>
2023-04-22 13:03:12 +0500

    Изменены тесты

--- job_parser/parser/tests/test_parser_models/test_favorite_vacancy_model.py
+++ job_parser/parser/tests/test_parser_models/test_favorite_vacancy_model.py
@@ -1,34 +1,223 @@
 from parser.models import FavouriteVacancy
-from parser.tests.TestAssertions import Assertions
 
 import pytest
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
+TEST_URL = "https://example.com/vacancy/1"
+TEST_TITLE = "Test title"
 
 
 @pytest.mark.django_db(transaction=True)
-class TestFavouriteVacancyModel:
-    def setup_data(self, test_user: User) -> tuple[FavouriteVacancy, dict]:
-        """Устанавливает тестовые данные.
-        Args:
-            test_user (User): Тестовый пользователь.
-        Returns:
-            tuple[FavouriteVacancy, dict]: Словарь с тестовыми данными и объект модели.
+class TestFavouriteVacancyModelPositive:
+    """Класс описывает позитивные тестовые случаи для модели FavouriteVacancy.
+
+    Декоратор `@pytest.mark.django_db` указывает pytest на необходимость использования
+    базы данных.
+    Параметр `transaction=True` указывает на использование транзакций для ускорения
+    и изоляции тестов.
+    Этот класс содержит тесты для проверки различных позитивных сценариев при создании
+    объектов модели FavouriteVacancy: возможность создания объекта модели,
+    проверка значений по умолчанию, проверка метода str, проверка метаданных модели,
+    проверка максимальной длины и уникальности полей.
+    """
+
+    def test_favourite_vacancy_model_creation(self) -> None:
+        """Тест проверяет создание объекта модели FavouriteVacancy.
+
+        Создается объект модели User и объект модели FavouriteVacancy с указанными
+        значениями полей.
+        Ожидается, что значения полей будут соответствовать указанным при создании
+        объекта.
         """
-        data = dict(
-            user=test_user,
-            url="http://test_url.com",
-            title="test_title",
+        user = User.objects.create(username="testuser")
+        favourite_vacancy = FavouriteVacancy.objects.create(
+            user=user, url=TEST_URL, title=TEST_TITLE
         )
-        FavouriteVacancy.objects.create(**data)
-        db_obj = FavouriteVacancy.objects.get(url=data.get("url", ""))
-        return db_obj, data
+        assert favourite_vacancy.user == user
+        assert favourite_vacancy.url == TEST_URL
+        assert favourite_vacancy.title == TEST_TITLE
+        assert str(favourite_vacancy) == TEST_TITLE
 
-    def test_favourite_vacancy_labels(self, test_user: User) -> None:
-        """Тестирует текстовые метки модели FavouriteVacancy."""
-        data = self.setup_data(test_user)
+    def test_favourite_vacancy_model_meta(self) -> None:
+        """Тест проверяет метаданные модели FavouriteVacancy.
 
-        Assertions.assert_labels(data[0], "user", "Пользователь")
-        Assertions.assert_labels(data[0], "title", "Вакансия")
-        Assertions.assert_unique(FavouriteVacancy, "url", "http://test_url.com")
-        Assertions.assert_nullable(FavouriteVacancy, data[1])
-        Assertions.assert_length(FavouriteVacancy, "title", 250)
+        Ожидается, что значения verbose_name и verbose_name_plural будут
+        соответствовать указанным в метаданных модели.
+        """
+        assert FavouriteVacancy._meta.verbose_name == "Избранная вакансия"
+        assert FavouriteVacancy._meta.verbose_name_plural == "Избранные вакансии"
+
+    def test_url_max_length(self) -> None:
+        """Тест проверяет максимальную длину поля url модели FavouriteVacancy.
+
+        Ожидается, что максимальная длина поля url будет равна 200 символам.
+        """
+        max_length = FavouriteVacancy._meta.get_field("url").max_length
+        assert max_length == 200
+
+    def test_title_max_length(self) -> None:
+        """Тест проверяет максимальную длину поля title модели FavouriteVacancy.
+
+        Ожидается, что максимальная длина поля title будет равна 250 символам.
+        """
+        max_length = FavouriteVacancy._meta.get_field("title").max_length
+        assert max_length == 250
+
+    def test_url_unique(self) -> None:
+        """Тест проверяет уникальность поля url модели FavouriteVacancy.
+
+        Ожидается, что поле url будет уникальным.
+        """
+        unique = FavouriteVacancy._meta.get_field("url").unique
+        assert unique is True
+
+    def test_url_blank(self) -> None:
+        """Тест проверяет возможность оставить поле url пустым.
+
+        Ожидается, что поле url не может быть пустым.
+        """
+        blank = FavouriteVacancy._meta.get_field("url").blank
+        assert blank is False
+
+    def test_title_blank(self) -> None:
+        """Тест проверяет возможность оставить поле title пустым.
+
+        Ожидается, что поле title не может быть пустым.
+        """
+        blank = FavouriteVacancy._meta.get_field("title").blank
+        assert blank is False
+
+    def test_url_null(self) -> None:
+        """Тест проверяет значение по умолчанию для поля url.
+
+        Ожидается, что значение по умолчанию для поля url будет равно False.
+        """
+        null = FavouriteVacancy._meta.get_field("url").null
+        assert null is False
+
+    def test_title_null(self) -> None:
+        """Тест проверяет значение по умолчанию для поля title.
+
+        Ожидается, что значение по умолчанию для поля title будет равно False.
+        """
+        null = FavouriteVacancy._meta.get_field("title").null
+        assert null is False
+
+
+@pytest.mark.django_db(transaction=True)
+class TestFavouriteVacancyModelNegative:
+    """Класс описывает негативные тестовые случаи для модели FavouriteVacancy.
+
+    Декоратор `@pytest.mark.django_db` указывает pytest на необходимость использования
+    базы данных.
+    Параметр `transaction=True` указывает на использование транзакций для ускорения
+    и изоляции тестов.
+    Этот класс содержит тесты для проверки различных негативных сценариев при создании
+    объектов модели FavouriteVacancy: невозможность создания объекта модели без
+    указания обязательных полей, проверка уникальности поля url, проверка максимальной
+    длины поля title, проверка каскадного удаления связанных объектов модели
+    FavouriteVacancy при удалении объекта модели User.
+    """
+
+    def test_create_favourite_vacancy_without_user(self) -> None:
+        """Тест проверяет попытку создания объекта модели FavouriteVacancy без
+        указания поля user.
+
+        Ожидается, что при попытке создать объект без указания поля user будет вызвано
+        исключение ValidationError.
+        """
+        with pytest.raises(ValidationError):
+            favourite_vacancy = FavouriteVacancy(url=TEST_URL, title=TEST_TITLE)
+            favourite_vacancy.full_clean()
+
+    def test_create_favourite_vacancy_without_url(self) -> None:
+        """Тест проверяет попытку создания объекта модели FavouriteVacancy без указания
+            поля url.
+
+        Создается объект модели User и объект модели FavouriteVacancy без указания
+        поля url.
+        Ожидается, что при попытке создать объект будет вызвано исключение
+        ValidationError.
+        """
+        with pytest.raises(ValidationError):
+            user = User.objects.create(username="testuser")
+            favourite_vacancy = FavouriteVacancy(user=user, title=TEST_TITLE)
+            favourite_vacancy.full_clean()
+
+    def test_create_favourite_vacancy_without_title(self) -> None:
+        """Тест проверяет попытку создания объекта модели FavouriteVacancy без указания
+        поля title.
+
+        Создается объект модели User и объект модели FavouriteVacancy без указания
+        поля title.
+        Ожидается, что при попытке создать объект будет вызвано исключение
+        ValidationError.
+        """
+        with pytest.raises(ValidationError):
+            user = User.objects.create(username="testuser")
+            favourite_vacancy = FavouriteVacancy(user=user, url=TEST_URL)
+            favourite_vacancy.full_clean()
+
+    def test_create_favourite_vacancy_with_duplicate_url(self) -> None:
+        """Тест проверяет попытку создания объекта модели FavouriteVacancy с
+        дублирующимся значением поля url.
+
+        Создается объект модели User и два объекта модели FavouriteVacancy с одинаковым
+        значением поля url.
+        Ожидается, что при попытке создать второй объект будет вызвано исключение
+        ValidationError.
+        """
+        user = User.objects.create(username="testuser")
+        FavouriteVacancy.objects.create(user=user, url=TEST_URL, title=TEST_TITLE)
+        with pytest.raises(ValidationError):
+            favourite_vacancy = FavouriteVacancy(
+                user=user,
+                url=TEST_URL,
+                title="Другая тестовая вакансия",
+            )
+            favourite_vacancy.full_clean()
+
+    def test_create_favourite_vacancy_with_too_long_title(self) -> None:
+        """Тест проверяет попытку создания объекта модели FavouriteVacancy с слишком
+        длинным значением поля title.
+
+        Создается объект модели User и объект модели FavouriteVacancy с длиной значения
+        поля title больше максимально допустимой.
+        Ожидается, что при попытке создать объект будет вызвано исключение
+        ValidationError.
+        """
+        with pytest.raises(ValidationError):
+            user = User.objects.create(username="testuser")
+            favourite_vacancy = FavouriteVacancy(
+                user=user, url=TEST_URL, title="a" * 251
+            )
+            favourite_vacancy.full_clean()
+
+    def test_create_favourite_vacancy_with_empty_title(self) -> None:
+        """Тест проверяет попытку создания объекта модели FavouriteVacancy с пустым
+        значением поля title.
+
+        Создается объект модели User и объект модели FavouriteVacancy с пустым
+        значением поля title.
+        Ожидается, что при попытке создать объект будет вызвано исключение
+        ValidationError.
+        """
+        with pytest.raises(ValidationError):
+            user = User.objects.create(username="testuser")
+            favourite_vacancy = FavouriteVacancy(user=user, url=TEST_URL, title="")
+            favourite_vacancy.full_clean()
+
+    def test_cascade_delete_on_user_delete(self):
+        """Тест проверяет каскадное удаление связанных объектов модели FavouriteVacancy
+        при удалении объекта модели User.
+
+        Создается объект модели User и связанный с ним объект модели FavouriteVacancy.
+        Ожидается, что при удалении объекта модели User связанный с ним объект модели
+        FavouriteVacancy также будет удален.
+        """
+        user = User.objects.create(username="testuser")
+        FavouriteVacancy.objects.create(user=user, url=TEST_URL, title=TEST_TITLE)
+        assert FavouriteVacancy.objects.count() == 1
+        user.delete()
+        assert FavouriteVacancy.objects.count() == 0
