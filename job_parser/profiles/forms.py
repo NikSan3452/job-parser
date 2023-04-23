0501b4e6b82ac3b28156982c7773ab1dc0fc66db
nik.san.3452@gmail.com <nik.san.3452@gmail.com>
2023-04-23 11:11:23 +0500

    Добавлена документация

--- job_parser/profiles/forms.py
+++ job_parser/profiles/forms.py
@@ -2,6 +2,17 @@ from django import forms
 
 
 class ProfileForm(forms.Form):
+    """
+    Класс формы профиля.
+
+    Этот класс наследуется от `forms.Form` и представляет собой форму профиля.
+    Он содержит три поля: `city`, `job` и `subscribe`.
+    Поле `city` является текстовым полем для ввода города.
+    Поле `job` является текстовым полем для ввода названия вакансии.
+    Поле `subscribe` является флажком для подписки на рассылку и не является
+    обязательным.
+    """
+
     city = forms.CharField(label="Город", widget=forms.TextInput())
     job = forms.CharField(label="Вакансия", widget=forms.TextInput())
     subscribe = forms.BooleanField(
