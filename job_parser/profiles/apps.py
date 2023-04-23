d33517b042ea219127bf2f71f1516ce4dad2007e
nik.san.3452@gmail.com <nik.san.3452@gmail.com>
2023-04-23 11:09:44 +0500

    Добавлена документация

--- job_parser/profiles/apps.py
+++ job_parser/profiles/apps.py
@@ -2,8 +2,24 @@ from django.apps import AppConfig
 
 
 class ProfilesConfig(AppConfig):
+    """
+    Класс конфигурации приложения `profiles`.
+
+    Этот класс наследуется от `AppConfig` и используется для конфигурации 
+    приложения profiles`.
+    Он содержит атрибуты `default_auto_field` и `name`.    
+    Также он содержит метод `ready`, который вызывается при запуске приложения и 
+    используется для импорта сигналов из модуля `profiles.signals`.
+    """
     default_auto_field = "django.db.models.BigAutoField"
     name = "profiles"
 
-    def ready(self):
+    def ready(self) -> None:
+        """
+        Метод вызывается при запуске приложения и используется для импорта сигналов
+        из модуля `profiles.signals`.
+
+        Returns:
+            None
+        """
         import profiles.signals
