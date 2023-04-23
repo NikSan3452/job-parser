b807d072f00d00497549f6eadfb164314fc91d88
nik.san.3452@gmail.com <nik.san.3452@gmail.com>
2023-04-23 11:04:16 +0500

    Добавлена документация

--- job_parser/profiles/tests/test_profiles_forms/test_profiles_forms.py
+++ job_parser/profiles/tests/test_profiles_forms/test_profiles_forms.py
@@ -1,5 +1,6 @@
-import pytest
 from parser.forms import SearchingForm
+
+import pytest
 
 
 class TestProfileForm:
@@ -17,6 +18,24 @@ class TestProfileForm:
         subscribe: str,
         validity: bool,
     ) -> None:
+        """
+        Тест проверяет валидность формы поиска.
+
+        Этот метод принимает строку с названием работы `job`, строку с названием
+        города `city`, логическое значение подписки `subscribe` и логическое значение
+        валидности `validity`.
+        Внутри метода создается экземпляр формы `SearchingForm` с данными из параметров.
+        Затем проверяется валидность формы и сравнивается с ожидаемым значением.
+
+        Args:
+            job (str): Название работы.
+            city (str): Название города.
+            subscribe (str): Значение подписки.
+            validity (bool): Ожидаемое значение валидности формы.
+
+        Returns:
+            None
+        """
         form = SearchingForm(
             data={
                 "job": job,
@@ -25,4 +44,5 @@ class TestProfileForm:
             }
         )
 
+        assert form.is_valid() is validity
         assert form.is_valid() is validity
