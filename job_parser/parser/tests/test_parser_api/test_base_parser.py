1e559525afff15b1e035fd63ebe88c56f47aed97
nik.san.3452@gmail.com <nik.san.3452@gmail.com>
2023-04-23 11:37:47 +0500

    Изменена документация

--- job_parser/parser/tests/test_parser_api/test_base_parser.py
+++ job_parser/parser/tests/test_parser_api/test_base_parser.py
@@ -8,20 +8,20 @@ import pytest
 
 @pytest.fixture
 def parser() -> Parser:
-    """Фикстура, которая возвращает экземпляр класса Parser.
+    """Фикстура возвращает экземпляр парсера.
 
     Returns:
-        Parser: Экземпляр класса Parser.
+        Parser: Экземпляр парсера.
     """
     return Parser()
 
 
 @pytest.fixture
 def params() -> tuple:
-    """Фикстура, которая создает тестовые параметры запроса.
+    """Фикстура возвращает кортеж параметров для тестов.
 
     Returns:
-        tuple: Параметры запроса.
+        tuple: Кортеж параметров для тестов.
     """
     items = None
     url = "http://127.0.0.1:8000"
@@ -87,17 +87,18 @@ async def mock_create_session(
     headers: dict | None = None,
     params: dict | None = None,
 ) -> list[dict]:
-    """Mock-метод, который возвращает тестовые данные для метода create_session.
+    """Функция-заглушка для создания сессии.
+
+    Возвращает фиктивный ответ с данными из функции get_data.
 
     Args:
-        connection (CreateConnection): Экземпляр CreateConnection, который передается
-        явно при вызыве этой функции, т.к первый аргумент в методе Parser.create_connection
-        это self.
-        url (str): URL - адрес.
-        headers (dict | None, optional): Заголовки. По-умолчанию None.
-        params (dict): Параметры запроса. По-умолчанию None.
+        connection (CreateConnection): Экземпляр соединения.
+        url (str): URL-адрес для запроса.
+        headers (dict | None): Заголовки запроса.
+        params (dict | None): Параметры запроса.
+
     Returns:
-        list[dict]: Тестовые данные.
+        list[dict]: Фиктивный ответ с данными из функции get_data.
     """
     data = get_data()
     # Кодируем данные
@@ -118,13 +119,23 @@ async def mock_create_session(
 
 
 class TestCreateConnection:
+    """Класс описывает тестовые случаи для создания соединения.
+
+    Этот класс содержит тесты для проверки успешного создания сессии.
+    """
+
     @pytest.mark.asyncio
     async def test_create_session_success(self, monkeypatch, params: tuple) -> None:
-        """Тестирует метод создания запросов к API.
+        """Тест проверяет успешное создание сессии.
+
+        Создается соединение и используется monkeypatch для замены метода
+        create_session на mock_create_session.
+        Вызывается метод create_session и проверяется код ответа, заголовки и
+        параметры запроса.
 
         Args:
-            monkeypatch (_type_): Фикстура для фиктивных запросов.
-            params (tuple): Фикстура с параметрами запроса.
+            monkeypatch: Фикстура pytest для временной замены атрибутов и методов.
+            params (tuple): Кортеж параметров для теста.
         """
         # создаем экземпляр класса CreateConnection
         connection = CreateConnection()
@@ -142,8 +153,11 @@ class TestCreateConnection:
         assert response.request.url.query.decode() == "text=python&limit=100&offset=0"
 
 
-class TestParser:
-    """Класс описывает тестовые случаи для класса Parser."""
+class TestParserPositive:
+    """Класс описывает позитивные тестовые случаи для парсера.
+
+    Этот класс содержит тесты для проверки получения вакансий с различными параметрами.
+    """
 
     @pytest.mark.asyncio
     @pytest.mark.parametrize(
@@ -162,14 +176,19 @@ class TestParser:
         items: str,
         expected_job_board: str,
     ) -> None:
-        """Тестирует метод получения вакансий.
+        """Тест проверяет получение вакансий с различными параметрами.
+
+        Используется monkeypatch для замены метода create_session на
+        mock_create_session.
+        Вызывается метод get_vacancies с различными параметрами и проверяется результат.
 
         Args:
-            monkeypatch (_type_): Фикстура для фиктивных запросов.
-            params (tuple): Параметры запроса.
-            parser (Parser): Экземпляр Parser.
-            items (str): Ключ словаря в возвращаемых API данных.
-            expected_job_board (str): Ожидаемая площадка.
+            monkeypatch: Фикстура pytest для временной замены атрибутов и методов.
+            params (tuple): Кортеж параметров для теста.
+            parser (Parser): Экземпляр парсера.
+            items (str): Параметр items для метода get_vacancies.
+            expected_job_board (str): Ожидаемое значение поля job_board в первой
+            вакансии.
         """
         # Имитируем метод создания запросов к API
         monkeypatch.setattr(CreateConnection, "create_session", mock_create_session)
@@ -190,15 +209,26 @@ class TestParser:
             assert isinstance(job_list, list)
             assert len(job_list) == 0
 
+
+class TestParserNegative:
+    """Класс описывает негативные тестовые случаи для парсера.
+
+    Этот класс содержит тесты для проверки поведения парсера при возникновении
+    исключений.
+    """
+
     @pytest.mark.asyncio
     async def test_get_vacancies_with_exception(
         self, parser: Parser, params: tuple
     ) -> None:
-        """Тестирует вызов исключения в методе получения вакансий.
+        """Тест проверяет поведение парсера при возникновении исключений.
+
+        Вызывается метод get_vacancies с невалидными параметрами и ожидается
+        возникновение исключения.
 
         Args:
             parser (Parser): Экземпляр парсера.
-            params (tuple): Параметры запроса.
+            params (tuple): Кортеж параметров для теста.
         """
         with pytest.raises(Exception):
             await parser.get_vacancies(
