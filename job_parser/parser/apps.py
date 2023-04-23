ce90c065ba122cd32e06e86939ff940dbd660d72
nik.san.3452@gmail.com <nik.san.3452@gmail.com>
2023-04-23 11:13:22 +0500

    Добавлена документация

--- job_parser/parser/apps.py
+++ job_parser/parser/apps.py
@@ -2,5 +2,13 @@ from django.apps import AppConfig
 
 
 class ParserConfig(AppConfig):
+    """
+    Класс конфигурации приложения `parser`.
+
+    Этот класс наследуется от `AppConfig` и используется для конфигурации приложения 
+    `parser`.
+    Он содержит атрибуты `default_auto_field` и `name`.
+    """
+
     default_auto_field = "django.db.models.BigAutoField"
     name = "parser"
