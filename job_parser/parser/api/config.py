5453403ba9e473c45e9e887fe5ceb567e2c57304
nik.san.3452@gmail.com <nik.san.3452@gmail.com>
2023-04-28 10:52:54 +0500

    Исправлены опечатки

--- job_parser/parser/api/config.py
+++ job_parser/parser/api/config.py
@@ -1,7 +1,6 @@
 import datetime
 import os
 from dataclasses import dataclass
-from typing import Optional
 
 from dotenv import load_dotenv
 
@@ -11,25 +10,25 @@ load_dotenv()
 class ParserConfig:
     """Этот класс хранит параметры запросов к API."""
 
-    superjob_domen: str = "https://api.superjob.ru"
+    superjob_domain: str = "https://api.superjob.ru"
     superjob_api_version: str = "2.0"
     superjob_api_path: str = "vacancies"
     superjob_secret_key: str | None = os.getenv("SUPERJOB_SECRET_KEY")
     superjob_headers: dict = {"x-api-app-id": superjob_secret_key}
-    superjob_url: str = f"{superjob_domen}/{superjob_api_version}/{superjob_api_path}/"
+    superjob_url: str = f"{superjob_domain}/{superjob_api_version}/{superjob_api_path}/"
 
-    headhunter_domen: str = "https://api.hh.ru"
+    headhunter_domain: str = "https://api.hh.ru"
     headhunter_api_path: str = "vacancies"
-    headhunter_url: str = f"{headhunter_domen}/{headhunter_api_path}"
+    headhunter_url: str = f"{headhunter_domain}/{headhunter_api_path}"
 
-    zarplata_domen: str = "https://api.zarplata.ru"
+    zarplata_domain: str = "https://api.zarplata.ru"
     zarplata_api_path: str = "vacancies"
-    zarplata_url: str = f"{zarplata_domen}/{zarplata_api_path}"
+    zarplata_url: str = f"{zarplata_domain}/{zarplata_api_path}"
 
-    trudvsem_domen: str = "http://opendata.trudvsem.ru/api"
+    trudvsem_domain: str = "http://opendata.trudvsem.ru/api"
     trudvsem_version: str = "v1"
     trudvsem_api_path: str = "vacancies"
-    trudvsem_url: str = f"{trudvsem_domen}/{trudvsem_version}/{trudvsem_api_path}"
+    trudvsem_url: str = f"{trudvsem_domain}/{trudvsem_version}/{trudvsem_api_path}"
 
 
 @dataclass
