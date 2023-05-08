215482e7b2524528f6db742ea3124c4f81974f4e
nik.san.3452@gmail.com <nik.san.3452@gmail.com>
2023-05-08 10:47:19 +0500

    Добавлены модели

--- job_parser/profiles/admin.py
+++ job_parser/profiles/admin.py
@@ -4,4 +4,4 @@ from profiles.models import Profile
 
 @admin.register(Profile)
 class CityAdmin(admin.ModelAdmin):
-    fields = ("user", "city", "job")
+    fields = ("user", "city", "job", "subscribe")
