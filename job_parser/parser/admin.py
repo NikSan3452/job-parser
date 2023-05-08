8c4705f5894c0e5e24b5baf23a16fb683b04df8e
nik.san.3452@gmail.com <nik.san.3452@gmail.com>
2023-05-08 10:46:56 +0500

    Добавлены модели

--- job_parser/parser/admin.py
+++ job_parser/parser/admin.py
@@ -1,8 +1,46 @@
 from django.contrib import admin
 
-from parser.models import City
+from parser.models import (
+    City,
+    FavouriteVacancy,
+    HiddenCompanies,
+    VacancyBlackList,
+    VacancyScraper,
+)
 
 
 @admin.register(City)
 class CityAdmin(admin.ModelAdmin):
-    fields = ("city",)
+    fields = ("city_id", "city")
+
+
+@admin.register(FavouriteVacancy)
+class FavouriteVacancyAdmin(admin.ModelAdmin):
+    fields = ("user", "url", "title")
+
+
+@admin.register(VacancyBlackList)
+class VacancyBlackListAdmin(admin.ModelAdmin):
+    fields = ("user", "url", "title")
+
+
+@admin.register(VacancyScraper)
+class VacancyScraperAdmin(admin.ModelAdmin):
+    fields = (
+        "job_board",
+        "url",
+        "title",
+        "description",
+        "city",
+        "salary",
+        "company",
+        "experience",
+        "type_of_work",
+        "remote",
+        "published_at",
+    )
+
+
+@admin.register(HiddenCompanies)
+class HiddenCompaniesAdmin(admin.ModelAdmin):
+    fields = ("user", "name")
