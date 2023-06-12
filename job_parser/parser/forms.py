923a16a04ce4e14f89f7815833b24699a5b93136
nik.san.3452@gmail.com <nik.san.3452@gmail.com>
2023-06-12 12:19:16 +0500

    Добавлен множественный выбор в полях опыт и площадка

--- job_parser/parser/forms.py
+++ job_parser/parser/forms.py
@@ -2,32 +2,14 @@ from django import forms
 
 
 class SearchingForm(forms.Form):
-    """
-    Класс формы поиска.
-
-    Этот класс наследуется от `forms.Form` и представляет собой форму поиска.
-    Он содержит несколько полей: `job`, `city`, `date_from`, `date_to`, `experience`,
-    `job_board`, `title_search` и `remote`.
-    Поле `job` является текстовым полем для ввода названия вакансии.
-    Поле `city` является текстовым полем для ввода города и не является обязательным.
-    Поля `date_from` и `date_to` являются полями для выбора даты и не являются
-    обязательными.
-    Поле `experience` является выпадающим списком для выбора опыта работы и
-    не является обязательным.
-    Поле `job_board` является выпадающим списком для выбора площадки для поиска вакансий
-    и не является обязательным.
-    Поле `title_search` является флажком для поиска вакансий только по названию
-    и не является обязательным.
-    Поле `remote` является флажком для поиска удаленных вакансий и
-    не является обязательным.
-    """
+    # TODO Добавить доки
 
     exp_values = (
-        (0, "Не имеет значения"),
-        (1, "Без опыта"),
-        (2, "от 1 до 3 лет"),
-        (3, "от 3 до 6 лет"),
-        (4, "от 6 лет"),
+        ("Не имеет значения", "Не имеет значения"),
+        ("Нет опыта", "Нет опыта"),
+        ("От 1 до 3 лет", "От 1 до 3 лет"),
+        ("От 3 до 6 лет", "От 3 до 6 лет"),
+        ("От 6 лет", "От 6 лет"),
     )
 
     job_board_values = (
@@ -36,12 +18,13 @@ class SearchingForm(forms.Form):
         ("SuperJob", "SuperJob"),
         ("Zarplata", "Zarplata"),
         ("Trudvsem", "Trudvsem"),
-        ("Habr career", "Habr career"),
+        ("Habr", "Habr"),
         ("Geekjob", "Geekjob"),
     )
 
-    job = forms.CharField(
+    title = forms.CharField(
         label="Что ищем ?",
+        required=False,
         max_length=250,
         widget=forms.TextInput(attrs={"placeholder": "Поиск"}),
     )
@@ -61,22 +44,23 @@ class SearchingForm(forms.Form):
         required=False,
         widget=forms.DateInput(format="%d/%m/%Y", attrs={"type": "date"}),
     )
-    experience = forms.ChoiceField(
+    experience = forms.MultipleChoiceField(
         label="Опыт работы",
         initial=exp_values[0],
         required=False,
         choices=exp_values,
-        widget=forms.Select(),
+        widget=forms.CheckboxSelectMultiple(),
     )
-    job_board = forms.ChoiceField(
+    job_board = forms.MultipleChoiceField(
         label="Площадка",
         initial=job_board_values[0],
         required=False,
         choices=job_board_values,
-        widget=forms.Select(),
+        widget=forms.CheckboxSelectMultiple(),
     )
+
     title_search = forms.BooleanField(
-        label="Искать в заголовках вакансий (чувствителен к регистру)",
+        label="Искать в заголовках вакансий",
         required=False,
         widget=forms.CheckboxInput(),
     )
