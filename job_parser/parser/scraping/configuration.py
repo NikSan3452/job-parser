4baad7053c3ffd645c61dc8ed87541a1fb4c19be
nik.san.3452@gmail.com <nik.san.3452@gmail.com>
2023-06-18 14:41:40 +0500

    Изменено

--- job_parser/parser/scraping/configuration.py
+++ job_parser/parser/scraping/configuration.py
@@ -10,10 +10,12 @@ class Config:
     GEEKJOB_DOMAIN: str = "https://geekjob.ru"
     GEEKJOB_URI: str = "vacancies"
     GEEKJOB_URL: str = f"{GEEKJOB_DOMAIN}/{GEEKJOB_URI}/"
+    geekjob_job_board: str = "Geekjob"
 
     HABR_DOMAIN: str = "https://career.habr.com"
     HABR_URI: str = "vacancies?sort=date&type=all&page="
     HABR_URL: str = f"{HABR_DOMAIN}/{HABR_URI}"
+    habr_job_board: str = "Habr"
 
     DOWNLOAD_DELAY: int = int(settings.DOWNLOAD_DELAY)
 
@@ -23,9 +25,18 @@ class Config:
     ua: UserAgent = UserAgent()
     headers: dict | None = None
 
-    def __post_init__(self):
+    def __post_init__(self) -> dict:
         self.headers = {"User-Agent": self.ua.random}
 
-    def update_headers(self):
+    def update_headers(self) -> dict:
+        """Обновляет заголовки запроса.
+
+        Метод обновляет значение атрибута `headers` объекта, устанавливая новое 
+        значение для ключа `User-Agent`. Возвращает обновленный словарь заголовков.
+
+        Returns:
+            dict: Обновленный словарь заголовков.
+
+        """
         self.headers = {"User-Agent": self.ua.random}
         return self.headers
