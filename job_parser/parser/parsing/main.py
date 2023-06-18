847f236d13d1b48ea3eeecfbadf39863c7b580fa
nik.san.3452@gmail.com <nik.san.3452@gmail.com>
2023-06-18 15:44:24 +0500

    Рефакторинг

--- job_parser/parser/parsing/main.py
+++ job_parser/parser/parsing/main.py
@@ -1,7 +1,10 @@
 import asyncio
 import time
 from parser.parsing.config import ParserConfig
-from parser.parsing.parsers import Headhunter, SuperJob, Trudvsem, Zarplata
+from parser.parsing.parsers.headhunter import Headhunter
+from parser.parsing.parsers.superjob import SuperJob
+from parser.parsing.parsers.trudvsem import Trudvsem
+from parser.parsing.parsers.zarplata import Zarplata
 from typing import Any
 
 from logger import logger, setup_logging
@@ -11,16 +14,17 @@ setup_logging()
 
 config = ParserConfig()
 
+
 class JobParser:
     """
     Класс для парсинга вакансий.
 
-    Класс содержит методы для парсинга вакансий с различных сайтов. При инициализации 
-    объекта класса создаются объекты классов `Headhunter`, `SuperJob`, `Zarplata` 
+    Класс содержит методы для парсинга вакансий с различных сайтов. При инициализации
+    объекта класса создаются объекты классов `Headhunter`, `SuperJob`, `Zarplata`
     и `Trudvsem`, которые используются для парсинга вакансий с соответствующих сайтов.
 
     Attributes:
-        hh (Headhunter): Объект класса `Headhunter` для парсинга вакансий с сайта 
+        hh (Headhunter): Объект класса `Headhunter` для парсинга вакансий с сайта
         HeadHunter.
         sj (SuperJob): Объект класса `SuperJob` для парсинга вакансий с сайта SuperJob.
         zp (Zarplata): Объект класса `Zarplata` для парсинга вакансий с сайта Zarplata.
@@ -37,7 +41,7 @@ class JobParser:
         """
         Асинхронный метод для парсинга вакансий.
 
-        Метод запускает параллельно 4 задачи для парсинга вакансий с сайтов 
+        Метод запускает параллельно 4 задачи для парсинга вакансий с сайтов
         HeadHunter, SuperJob, Zarplata и Trudvsem.
 
         Returns:
@@ -55,12 +59,13 @@ class JobParser:
             task4,
         )
 
+
 async def start() -> Any:
     """
     Асинхронная функция для запуска парсинга вакансий.
 
-    Функция создает объект класса `JobParser`, затем вызывает его метод 
-    `parse_vacancies` для парсинга. В конце работы функции выводится сообщение с 
+    Функция создает объект класса `JobParser`, затем вызывает его метод
+    `parse_vacancies` для парсинга. В конце работы функции выводится сообщение с
     информацией о времени, затраченном на работу парсера.
 
     Returns:
@@ -68,7 +73,7 @@ async def start() -> Any:
     """
     start = time.time()
     parser = JobParser()
-    
+
     await parser.parse_vacancies()
 
     end = time.time() - start
