ae4a557ac25836384cb30aba6e209d5262ef55ed
nik.san.3452@gmail.com <nik.san.3452@gmail.com>
2023-06-18 12:38:30 +0500

    Обновлено

--- job_parser/parser/urls.py
+++ job_parser/parser/urls.py
@@ -1,45 +1,61 @@
 from django.urls import path
 
-from . import views
+from .views.blacklist import (
+    AddToBlackListView,
+    ClearBlackList,
+    DeleteFromBlacklistView,
+)
+from .views.favourite import (
+    AddToFavouritesView,
+    ClearFavouriteList,
+    DeleteFromFavouritesView,
+)
+from .views.hiddenCompanies import (
+    ClearHiddenCompaniesList,
+    DeleteFromHiddenCompaniesView,
+    HideCompanyView,
+)
+from .views.home import HomePageView
+from .views.list import VacancyListView
 
 urlpatterns = [
-    path("", views.HomePageView.as_view(), name="home"),
-    path("list/", views.VacancyListView.as_view(), name="list"),
-    path("favourite/", views.AddVacancyToFavouritesView.as_view(), name="favourite"),
-    path(
-        "clear-favourite-list/",
-        views.ClearVacancyFavouriteList.as_view(),
-        name="clear_favourite_list",
-    ),
+    path("", HomePageView.as_view(), name="home"),
+    path("list/", VacancyListView.as_view(), name="list"),
+    path("favourite/", AddToFavouritesView.as_view(), name="favourite"),
     path(
         "delete-favourite/",
-        views.DeleteVacancyFromFavouritesView.as_view(),
+        DeleteFromFavouritesView.as_view(),
         name="delete_favourite",
     ),
     path(
+        "clear-favourite-list/",
+        ClearFavouriteList.as_view(),
+        name="clear_favourite_list",
+    ),
+    path(
         "add-to-black-list/",
-        views.AddVacancyToBlackListView.as_view(),
+        AddToBlackListView.as_view(),
         name="add_to_black_list",
     ),
     path(
-        "clear-blacklist-list/",
-        views.ClearVacancyBlackList.as_view(),
-        name="clear_blacklist_list",
-    ),
-    path("hide-company/", views.HideCompanyView.as_view(), name="hide_company"),
-    path(
-        "clear-hidden-companies-list/",
-        views.ClearHiddenCompaniesList.as_view(),
-        name="clear_hidden_companies_list",
-    ),
-    path(
         "delete-from-blacklist/",
-        views.DeleteVacancyFromBlacklistView.as_view(),
+        DeleteFromBlacklistView.as_view(),
         name="delete_from_blacklist",
     ),
     path(
+        "clear-blacklist-list/",
+        ClearBlackList.as_view(),
+        name="clear_blacklist_list",
+    ),
+    path("hide-company/", HideCompanyView.as_view(), name="hide_company"),
+    path(
         "delete-from-hidden-companies/",
-        views.DeleteFromHiddenCompaniesView.as_view(),
+        DeleteFromHiddenCompaniesView.as_view(),
         name="delete_from_hidden_companies",
+    ),
+    path(
+        "clear-hidden-companies-list/",
+        ClearHiddenCompaniesList.as_view(),
+        name="clear_hidden_companies_list",
     ),
 ]
