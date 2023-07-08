141f54ba4a5b2b1bf512d50d622db145450235e8
nik.san.3452@gmail.com <nik.san.3452@gmail.com>
2023-07-08 15:14:24 +0500

    Рефакторинг

--- job_parser/parser/scraping/run.py
+++ job_parser/parser/scraping/run.py
@@ -1,18 +1,16 @@
 import asyncio
 import time
-
-import aiohttp
-from loguru import logger
-from logger import setup_logging
-
 from parser.scraping.configuration import Config
-from parser.scraping.scrapers.geekjob import GeekjobParser
-from parser.scraping.scrapers.habr import HabrParser
+from parser.scraping.scrapers.geekjob import GeekjobScraper
+from parser.scraping.scrapers.habr import HabrScraper
+
+from logger import setup_logging
+from loguru import logger
 
 setup_logging()
 
 
-class StartParsers:
+class StartScrapers:
     """Класс предназначен для запуска скраперов сайтов поиска работы.
 
     Args:
@@ -20,18 +18,16 @@ class StartParsers:
         config (Config): Объект класса Config, содержит настройки для парсера.
 
     """
-    def __init__(self, session: aiohttp.ClientSession, config: Config) -> None:
-        self.session = session
+
+    def __init__(self, config: Config) -> None:
         self.config = config
+        
 
-        self.geekjob_parser = GeekjobParser(config, session)
-        self.habr_parser = HabrParser(config, session)
-
-    async def run_parsers(self) -> asyncio.Future[list]:
+    async def run_scrapers(self) -> asyncio.Future[list]:
         """Запускает скраперы сайтов поиска работы.
 
-        Создает задачи для сохранения данных с сайтов geekjob.ru и career.habr.com и 
-        ожидает их завершения. Возвращает список результатов выполнения задач.
+        Создает задачи для сохранения данных с сайтов и ожидает их завершения.
+        Возвращает список результатов выполнения задач.
 
         Returns:
             asyncio.Future[list]: Список результатов выполнения задач.
@@ -39,8 +35,9 @@ class StartParsers:
         """
         tasks: list[asyncio.Future] = []
 
-        task1 = asyncio.create_task(self.geekjob_parser.save_data())
-        task2 = asyncio.create_task(self.habr_parser.save_data())
+        task1 = asyncio.create_task(self.geekjob_scraper.save())
+        task2 = asyncio.create_task(self.habr_scraper.save())
+
         tasks.append(task1)
         tasks.append(task2)
         return await asyncio.gather(*tasks)
@@ -49,23 +46,23 @@ class StartParsers:
 async def run():
     """Запускает скрапер сайтов поиска работы.
 
-    Создает объект сессии aiohttp и объект класса Config. Затем создает объект класса 
-    StartParsers и вызывает его метод run_parsers. 
+    Создает объект класса Config со всеми необходимыми параметрами,
+    затем создает экземпляр класса StartParsers с переданными ему параметрами
+    и вызывает его метод run_parsers.
     Выводит информацию о времени работы скрапера в лог.
 
     """
-    async with aiohttp.ClientSession() as session:
-        config = Config()
-        parser = StartParsers(session, config)
+    config = Config()
+    parser = StartScrapers(config)
 
-        start_time = time.time()
-        logger.info("Скрапер запущен")
+    start_time = time.time()
+    logger.info("Скрапер запущен")
 
-        await parser.run_parsers()
+    await parser.run_scrapers()
 
-        logger.info("Работа скрапера завершена")
-        finish_time = time.time() - start_time
-        logger.debug(f"Затраченное на работу скрапера время: {round(finish_time, 2)}")
+    logger.info("Работа скрапера завершена")
+    finish_time = time.time() - start_time
+    logger.debug(f"Затраченное на работу скрапера время: {round(finish_time, 2)}")
 
 
 if __name__ == "__main__":
