91e6c29e65ba5341528a27196ea50bb5c5955609
nik.san.3452@gmail.com <nik.san.3452@gmail.com>
2023-07-08 15:14:40 +0500

    Рефакторинг

--- job_parser/parser/scraping/scrapers/base.py
+++ job_parser/parser/scraping/scrapers/base.py
@@ -1,102 +1,80 @@
 import abc
-import asyncio
 import datetime
-from parser.models import Vacancies
-from parser.scraping.configuration import Config
-from parser.scraping.fetching import Fetcher
+from dataclasses import asdict, dataclass
 from typing import AsyncGenerator
 
 from bs4 import BeautifulSoup
-from django.db import DatabaseError, Error, IntegrityError, ProgrammingError
-from loguru import logger
+
+
+@dataclass
+class Vacancy:
+    job_board: str
+    url: str
+    title: str
+    city: str
+    description: str
+    salary_from: int
+    salary_to: int
+    salary_currency: str
+    company: str
+    experience: str
+    schedule: str
+    remote: bool
+    published_at: str
 
 
 class Scraper(abc.ABC):
-    """Класс Scraper предназначен для извлечения информации о вакансиях с сайтов 
+    """Класс Scraper предназначен для извлечения информации о вакансиях с сайтов
     поиска работы.
 
     Args:
-        fetcher (Fetcher): Объект класса Fetcher, который используется для получения 
+        fetcher (Fetcher): Объект класса Fetcher, который используется для получения
         данных с сайтов поиска работы.
         config (Config): Объект класса Config, который содержит настройки для парсера.
         job_board (str): Название сайта поиска работы.
     """
-    def __init__(self, fetcher: Fetcher, config: Config, job_board: str) -> None:
-        self.fetcher = fetcher
-        self.config = config
+
+    def __init__(self, job_board: str) -> None:
         self.job_board = job_board
 
-    async def get_vacancy_details(self, page: tuple) -> AsyncGenerator:
+    async def get_vacancy_details(
+        self, page: tuple[str, str]
+    ) -> AsyncGenerator[dict, None]:
         """Извлекает информацию о вакансии со страницы вакансии.
 
-        Метод принимает кортеж из двух элементов: HTML-кода страницы и URL-адреса. 
-        Затем он создает объект BeautifulSoup из HTML-кода и использует абстрактные 
-        методы для извлечения информации о вакансии. Полученная информация сохраняется 
-        в словаре и возвращается как результат генератора.
+        Метод принимает кортеж из двух элементов: HTML-кода страницы и URL-адреса.
+        Затем он создает объект BeautifulSoup из HTML-кода и использует абстрактные
+        методы для извлечения информации о вакансии. Полученная информация сохраняется
+        в экземпляре класса Vacancy и возвращается как результат генератора.
 
         Args:
-            page (tuple): Кортеж из двух элементов: HTML-кода страницы и URL-адреса.
+            page (tuple[str, str]): Кортеж из двух элементов: HTML-кода страницы 
+            и URL-адреса.
 
         Yields:
             dict: Словарь с информацией о вакансии.
 
         """
-        vacancy: dict = {}
-
         html, url = page
         soup = BeautifulSoup(html, "lxml")
-        vacancy["job_board"] = self.job_board
-        vacancy["url"] = url
 
-        (
-            vacancy["title"],
-            vacancy["city"],
-            vacancy["description"],
-            vacancy["salary_from"],
-            vacancy["salary_to"],
-            vacancy["salary_currency"],
-            vacancy["company"],
-            vacancy["experience"],
-            vacancy["schedule"],
-            vacancy["remote"],
-            vacancy["published_at"],
-        ) = await asyncio.gather(
-            self.get_title(soup),
-            self.get_city(soup),
-            self.get_description(soup),
-            self.get_salary_from(soup),
-            self.get_salary_to(soup),
-            self.get_salary_currency(soup),
-            self.get_company(soup),
-            self.get_experience(soup),
-            self.get_schedule(soup),
-            self.get_remote(),
-            self.get_published_at(soup),
+        vacancy = Vacancy(
+            job_board=self.job_board,
+            url=url,
+            title=await self.get_title(soup),
+            city=await self.get_city(soup),
+            description=await self.get_description(soup),
+            salary_from=await self.get_salary_from(soup),
+            salary_to=await self.get_salary_to(soup),
+            salary_currency=await self.get_salary_currency(soup),
+            company=await self.get_company(soup),
+            experience=await self.get_experience(soup),
+            schedule=await self.get_schedule(soup),
+            remote=await self.get_remote(),
+            published_at=await self.get_published_at(soup),
         )
-        yield vacancy
 
-    async def save_data(self) -> int:
-        """Сохраняет информацию о вакансиях в базе данных.
-
-        Метод использует объект fetcher для получения ссылок на вакансии 
-        и страниц вакансий. Затем он использует метод get_vacancy_details для 
-        извлечения информации о вакансиях и сохраняет ее в базе данных с помощью 
-        модели Vacancies. Возвращает количество сохраненных вакансий.
-
-        Returns:
-            int: Количество сохраненных вакансий.
-
-        """
-        links = await self.fetcher.fetch_vacancy_links(self.job_board)
-        vacancy_counter = 0
-        async for page in self.fetcher.fetch_vacancy_pages(links):
-            async for vacancy in self.get_vacancy_details(page):
-                try:
-                    await Vacancies.objects.aget_or_create(**vacancy)
-                except (Error, IntegrityError, DatabaseError, ProgrammingError) as exc:
-                    logger.exception(exc)
-                vacancy_counter += 1
-        return vacancy_counter
+        yield asdict(vacancy)
 
     @abc.abstractmethod
     async def get_title(self, soup: BeautifulSoup) -> str:
@@ -223,7 +201,7 @@ class Scraper(abc.ABC):
             soup (BeautifulSoup): Объект BeautifulSoup со страницей вакансии.
 
         Returns:
-            datetime.date | None: Дата публикации вакансии или None, если дата 
+            datetime.date | None: Дата публикации вакансии или None, если дата
             публикации не указана.
         """
         pass
