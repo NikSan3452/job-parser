03b65e75167715a1e44854a29410c3cb822e8979
nik.san.3452@gmail.com <nik.san.3452@gmail.com>
2023-08-13 15:52:49 +0500

    Рефакторинг

--- job_parser/parser/parsing/main.py
+++ job_parser/parser/parsing/main.py
@@ -10,18 +10,8 @@ config = ParserConfig()
 
 class JobParser:
     """
-    Класс для парсинга вакансий.
+    Класс содержит методы для парсинга вакансий с различных сайтов.
 
-    Класс содержит методы для парсинга вакансий с различных сайтов. При инициализации
-    объекта класса создаются объекты классов `Headhunter`, `SuperJob`, `Zarplata`
-    и `Trudvsem`, которые используются для парсинга вакансий с соответствующих сайтов.
-
-    Attributes:
-        (Headhunter): Объект класса `Headhunter` для парсинга вакансий с сайта
-        HeadHunter.
-        (SuperJob): Объект класса `SuperJob` для парсинга вакансий с сайта SuperJob.
-        (Zarplata): Объект класса `Zarplata` для парсинга вакансий с сайта Zarplata.
-        (Trudvsem): Объект класса `Trudvsem` для парсинга вакансий с сайта Trudvsem.
     """
     @config.utils.timeit
     async def parse_headhunter(self) -> None:
