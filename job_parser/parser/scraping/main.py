2108f80d62c00751fd0881758e2f63497bd38085
nik.san.3452@gmail.com <nik.san.3452@gmail.com>
2023-08-13 15:53:10 +0500

    Рефакторинг

--- job_parser/parser/scraping/main.py
+++ job_parser/parser/scraping/main.py
@@ -1,58 +1,31 @@
-import asyncio
-import time
-
-from logger import logger, setup_logging
+from logger import setup_logging
 
 from parser.scraping.configuration import Config
 
 setup_logging()
 
+config = Config()
+
 
 class StartScrapers:
-    """Класс предназначен для запуска скраперов сайтов поиска работы.
+    """Класс предназначен для запуска скраперов сайтов поиска работы."""
 
-    Args:
-        config (Config): Объект класса Config, содержит настройки для парсера.
-
-    """
-
-    def __init__(self, config: Config) -> None:
-        self.config = config
-
-    async def start(self) -> None:
-        """Запускает скраперы сайтов поиска работы.
-
-        Создает задачи для сохранения данных с сайтов и ожидает их завершения.
-
-        Returns: None
+    @config.utils.timeit
+    async def scrape_habr(self) -> None:
         """
-        tasks = [
-            asyncio.create_task(scraper.scrape()) for scraper in self.config.scrapers
-        ]
-        await asyncio.gather(*tasks)
+        Асинхронный метод для скрапинга вакансий с сайта Habr.
 
+        Returns:
+            None
+        """
+        await config.habr_scraper.scrape()
 
-async def main():
-    """Запускает скрапер сайтов поиска работы.
+    @config.utils.timeit
+    async def scrape_geekjob(self) -> None:
+        """
+        Асинхронный метод для скрапинга вакансий с сайта Geekjob.
 
-    Создает объект класса Config со всеми необходимыми параметрами,
-    затем создает экземпляр класса StartParsers с переданными ему параметрами
-    и вызывает его метод run_parsers.
-    Выводит информацию о времени работы скрапера в лог.
-
-    """
-    config = Config()
-    scrapers = StartScrapers(config)
-
-    start_time = time.time()
-    logger.info("Скрапер запущен")
-
-    await scrapers.start()
-
-    logger.info("Работа скрапера завершена")
-    finish_time = time.time() - start_time
-    logger.debug(f"Затраченное на работу скрапера время: {round(finish_time, 2)}")
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
+        Returns:
+            None
+        """
+        await config.habr_scraper.scrape()
